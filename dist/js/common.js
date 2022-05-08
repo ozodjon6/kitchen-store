2b12c508ae956c209573f92d79a6f93e7841b6c9
Ozodbek <o.jumaev@safenetpay.com>
2022-05-08 17:18:57 +0500

    feat: add slider

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,31 +1,18 @@
-// const openBtn = document.querySelector(".header-page__btn:last-child");
-// const openSignBtn = document.querySelector(".header-page__btn:first-child");
-// const popupPage = document.querySelector(".popup-page");
-// const signPage = document.querySelector(".sign-page");
-// const overlay = document.querySelector(".overlay");
-// const closeBtn = document.querySelectorAll(".close-btn");
-
-
-// openBtn.addEventListener("click", function() {
-//     popupPage.classList.add("active")
-//     overlay.classList.add("active")
-// })
-
-// openSignBtn.addEventListener("click", function() {
-//     signPage.classList.add("active")
-//     overlay.classList.add("active")
-// })
-
-// for (let i = 0; i < closeBtn.length; i++) {
-//     closeBtn[i].addEventListener("click", function() {
-//         popupPage.classList.remove("active");
-//         signPage.classList.remove("active");
-//         overlay.classList.remove("active")
-//     })
-// }
-
-// overlay.addEventListener("click", function() {
-//     popupPage.classList.remove("active");
-//     signPage.classList.remove("active");
-//     this.classList.remove("active")
-// })
+// Slider
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    autplay: false,
+    cssMode: false,
+    loop: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    mousewheel: false,
+    keyboard: true,
+});
