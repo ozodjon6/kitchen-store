630115666ada6191714d2663a931ffa04582a3ea
Ozodbek <o.jumaev@safenetpay.com>
2022-05-19 15:19:49 +0500

    fix submenu

--- dist/js/common.js
+++ dist/js/common.js
@@ -256,7 +256,7 @@ findVideos();
 
     controlls.forEach(function (control) {
       control.addEventListener("click", function (e) {
-        e.preventDefault();
+        // e.preventDefault();
 
         const filterName = control.getAttribute("data-filter");
 
@@ -314,3 +314,15 @@ findVideos();
 // wow-min js
 
 new WOW().init();
+
+// Bootstrap tab
+
+$(document).ready(function(){
+  $(".nav-tabs a").click(function(){
+    $(this).tab('show');
+  });
+  $('.nav-tabs a').on('shown.bs.tab', function(event){
+    var x = $(event.target).text();         // active tab
+    var y = $(event.relatedTarget).text();  // previous tab
+  });
+});
